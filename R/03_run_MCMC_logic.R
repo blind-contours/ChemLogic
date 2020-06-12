a564eec7d86bb76cb8c2c02d990f1a1c4cd63716
David McCoy <david_mccoy@berkeley.edu>
2020-06-12 10:34:22 -0700

    Built out to collecting and plotting the variables from MCMC, couples and triples. Moving on to extracting trees.

--- R/03_run_MCMC_logic.R
+++ R/03_run_MCMC_logic.R
@@ -9,120 +9,215 @@
 #' @export
 #'
 
+run_logic_MCMC <- function(
+                           outcome_label,
+                           filter_type,
+                           binary_data,
+                           subgroups,
+                           subgroup_label,
+                           mx_size,
+                           ntrees,
+                           n_burn_in,
+                           niter,
+                           hyperparameters = log(2),
+                           outcome_permuted = FALSE,
+                           top_fraction = 0.2) {
+  if (subgroups) {
+    binary_data_split <- binary_data %>%
+      filter(!!(outcome_label) == 1) %>%
+      group_split((!!(subgroup_label)))
 
-logic_MCMC <- function(outcome,
-                       exposure,
-                       mx_size,
-                       ntrees,
-                       n_burn_in,
-                       niter,
-                       hyperparameters,
-                       outcome_permuted,
-                       top_fraction)
-{
+    control_data <- binary_data %>%
+      filter(!!(outcome_label) != 1)
 
-  if(outcome_permuted){
-    outcome <- sample(AR_pubchem_data$Label, replace = TRUE)
+    subgroup_dfs <- map(.x = binary_data_split, ~ rbind(.x, control_data))
+
+    subgroup_exposures <- map(.x = subgroup_dfs, ~ dplyr::select(
+      .x,
+      contains(filter_type)
+    ))
+
+    subgroup_outcomes <- map(.x = subgroup_dfs, ~ dplyr::select(
+      .x,
+      !!(subgroup_label)
+    ))
+
+    subgroup_outcomes <- map(.x = subgroup_outcomes, droplevels)
+
+    check_outcome <- function(x) {
+      if(is.numeric(x[[1]])) {
+        x[[1]] <- ifelse(x[[1]] >= 1, 1,0)
+      }
+      if(is.factor(x[[1]])) {
+        levels(x[[1]]) <- c(0,1)
+        x[[1]] <- as.numeric(x[[1]])
+        x[[1]] <- x[[1]]  - 1
+      }
+
+      return(x[[1]])
+    }
+
+    subgroup_outcomes <- map(.x = subgroup_outcomes, check_outcome)
+
+    subgroup_logreg_MCMC_results <- map2(
+      .x = subgroup_exposures,
+      .y = subgroup_outcomes,
+      .f = ~ logreg(
+        resp = .y,
+        bin = .x,
+        select = 7,
+        type = 3,
+        nleaves = mx_size,
+        ntrees = 1,
+        mc.control = logreg.mc.control(
+          nburn = n_burn_in,
+          niter = niter,
+          hyperpars = hyperparameters
+        )
+      )
+    )
+  } else {
+    subgroup_logreg_MCMC_results <- NULL
   }
 
-  fit_MCMC <- logreg(resp = outcome,
-                     bin=exposure,
-                     type = 3,
-                     select = 7,
-                     nleaves = mx_size,
-                     ntrees = ntrees,
-                     mc.control = logreg.mc.control(nburn=n_burn_in,
-                                                    niter=niter,
-                                                    hyperpars=hyperparameters))
 
-  msz_by_iter_MCMC <- fit_MCMC$size
-  ni_in_model_MCMC <- fit_MCMC$single
-  nij_in_model_MCMC <- as.matrix(fit_MCMC$double)
-  nijk_in_model_MCMC <- as.array(fit_MCMC$triple)
+  outcomes <- binary_data %>%
+    dplyr::select(
+      !!(outcome_label)
+    )
 
-  ni_in_model_MCMC_structures <- cbind(pubchem_fingerprints_cleaned, ni_in_model_MCMC)
-  ni_in_model_MCMC_structures$fraction <- ni_in_model_MCMC_structures$ni_in_model_MCMC/niter
+  mol_features <- dplyr::select(
+    binary_data,
+    contains(filter_type)
+  )
+
+  main_outcome_logreg_MCMC_results <- logreg(
+    resp = outcomes[[1]],
+    bin = mol_features,
+    select = 7,
+    type = 3,
+    nleaves = mx_size,
+    ntrees = ntrees,
+    mc.control = logreg.mc.control(
+      nburn = n_burn_in,
+      niter = niter,
+      hyperpars = hyperparameters
+    )
+  )
+
+  return(list(
+    MCMC_subgroups_analysis = subgroup_logreg_MCMC_results,
+    MCMC_main_outcomes_analysis = main_outcome_logreg_MCMC_results
+  ))
+}
+
+
+
+collect_logic_MCMC_results <- function(MCMC_results, fingerprints, niter, fingerprint_idx, binary_data, filter_type, top_fraction) {
+
+  binary_exposure_data <- binary_data %>% dplyr::select(
+    contains(filter_type))
+
+
+  msz_by_iter_MCMC <- MCMC_results$size
+  ni_in_model_MCMC <- MCMC_results$single
+  nij_in_model_MCMC <- as.matrix(MCMC_results$double)
+  nijk_in_model_MCMC <- as.array(MCMC_results$triple)
+
+  ni_in_model_MCMC_structures <- cbind(fingerprints, ni_in_model_MCMC)
+  ni_in_model_MCMC_structures$fraction <- ni_in_model_MCMC_structures$ni_in_model_MCMC / niter
   ni_in_model_MCMC_structures_ordered <- arrange(ni_in_model_MCMC_structures, desc(fraction))
 
-  #ij in same trees
+  # ij in same trees
 
-  rownames(nij_in_model_MCMC) <- pubchem_fingerprints_cleaned[,2]
-  colnames(nij_in_model_MCMC) <- pubchem_fingerprints_cleaned[,2]
+  rownames(nij_in_model_MCMC) <- fingerprints[, fingerprint_idx]
+  colnames(nij_in_model_MCMC) <- fingerprints[, fingerprint_idx]
 
-  nij_in_model_MCMC_collapsed <- setNames(melt(nij_in_model_MCMC), c('Bit Structure 1',
-                                                                     'Bit Structure 2',
-                                                                     'count'))
+  nij_in_model_MCMC_collapsed <- setNames(reshape2::melt(nij_in_model_MCMC), c(
+    "Bit Structure 1",
+    "Bit Structure 2",
+    "count"
+  ))
 
-  nij_in_model_MCMC_collapsed$fraction <- nij_in_model_MCMC_collapsed$count/niter
+  nij_in_model_MCMC_collapsed$fraction <- nij_in_model_MCMC_collapsed$count / niter
   nij_in_model_MCMC_collapsed_top <- subset(nij_in_model_MCMC_collapsed, fraction > top_fraction)
-  #nij_in_model_MCMC_collapsed_top
+  # nij_in_model_MCMC_collapsed_top
 
-  chem_obs <- dim(AR_pubchem_data)[1]
-  pij <- colSums(exposure)/chem_obs
-  names(pij) <- pubchem_fingerprints_cleaned[,2]
+  chem_obs <- dim(binary_exposure_data)[1]
+  pij <- colSums(binary_exposure_data) / chem_obs
+  names(pij) <- fingerprints[, fingerprint_idx]
   pij <- as.data.frame(pij)
 
   pij_calcs <- merge(nij_in_model_MCMC_collapsed_top,
-                     pij,
-                     by.x = "Bit Structure 1",
-                     by.y = "row.names")
+    pij,
+    by.x = "Bit Structure 1",
+    by.y = "row.names"
+  )
 
   pij_calcs <- merge(pij_calcs,
-                     pij,
-                     by.x = "Bit Structure 2",
-                     by.y = "row.names")
+    pij,
+    by.x = "Bit Structure 2",
+    by.y = "row.names"
+  )
 
-  names(pij_calcs)[names(pij_calcs) == 'pij.y'] <- 'pij Bit Structure 1'
-  names(pij_calcs)[names(pij_calcs) == 'pij.x'] <- 'pij Bit Structure 2'
+  names(pij_calcs)[names(pij_calcs) == "pij.y"] <- "pij Bit Structure 1"
+  names(pij_calcs)[names(pij_calcs) == "pij.x"] <- "pij Bit Structure 2"
 
   pij_calcs$expected_fraction <- pij_calcs$`pij Bit Structure 1` * pij_calcs$`pij Bit Structure 2`
   pij_calcs$ratio <- pij_calcs$fraction / pij_calcs$expected_fraction
 
-  #ijk combinations
+  # ijk combinations
 
-  dimnames(nijk_in_model_MCMC)[[1]] <- pubchem_fingerprints_cleaned[,2]
-  dimnames(nijk_in_model_MCMC)[[2]] <- pubchem_fingerprints_cleaned[,2]
-  dimnames(nijk_in_model_MCMC)[[3]] <- pubchem_fingerprints_cleaned[,2]
+  dimnames(nijk_in_model_MCMC)[[1]] <- fingerprints[, fingerprint_idx]
+  dimnames(nijk_in_model_MCMC)[[2]] <- fingerprints[, fingerprint_idx]
+  dimnames(nijk_in_model_MCMC)[[3]] <- fingerprints[, fingerprint_idx]
 
-  nijk_in_model_MCMC_collapsed <- setNames(melt(nijk_in_model_MCMC), c('Bit Structure 1',
-                                                                       'Bit Structure 2',
-                                                                       'Bit Structure 3',
-                                                                       'count'))
+  nijk_in_model_MCMC_collapsed <- setNames(reshape2::melt(nijk_in_model_MCMC), c(
+    "Bit Structure 1",
+    "Bit Structure 2",
+    "Bit Structure 3",
+    "count"
+  ))
 
-  nijk_in_model_MCMC_collapsed$fraction <- nijk_in_model_MCMC_collapsed$count/niter
+  nijk_in_model_MCMC_collapsed$fraction <- nijk_in_model_MCMC_collapsed$count / niter
   nijk_in_model_MCMC_collapsed_top <- subset(nijk_in_model_MCMC_collapsed, fraction > top_fraction)
-  #nijk_in_model_MCMC_collapsed_top
+  # nijk_in_model_MCMC_collapsed_top
 
   pijk_calcs <- merge(nijk_in_model_MCMC_collapsed_top,
-                      pij,
-                      by.x = "Bit Structure 1",
-                      by.y = "row.names")
+    pij,
+    by.x = "Bit Structure 1",
+    by.y = "row.names"
+  )
 
-  names(pijk_calcs)[names(pijk_calcs) == 'pij'] <- 'pijk Bit Structure 1'
-
-  pijk_calcs <- merge(pijk_calcs,
-                      pij,
-                      by.x = "Bit Structure 2",
-                      by.y = "row.names")
-
-  names(pijk_calcs)[names(pijk_calcs) == 'pij'] <- 'pijk Bit Structure 2'
+  names(pijk_calcs)[names(pijk_calcs) == "pij"] <- "pijk Bit Structure 1"
 
   pijk_calcs <- merge(pijk_calcs,
-                      pij,
-                      by.x = "Bit Structure 3",
-                      by.y = "row.names")
+    pij,
+    by.x = "Bit Structure 2",
+    by.y = "row.names"
+  )
 
-  names(pijk_calcs)[names(pijk_calcs) == 'pij'] <- 'pijk Bit Structure 3'
+  names(pijk_calcs)[names(pijk_calcs) == "pij"] <- "pijk Bit Structure 2"
+
+  pijk_calcs <- merge(pijk_calcs,
+    pij,
+    by.x = "Bit Structure 3",
+    by.y = "row.names"
+  )
+
+  names(pijk_calcs)[names(pijk_calcs) == "pij"] <- "pijk Bit Structure 3"
 
   pijk_calcs$expected_fraction <- pijk_calcs$`pijk Bit Structure 1` *
-    pijk_calcs$`pijk Bit Structure 2`*
+    pijk_calcs$`pijk Bit Structure 2` *
     pijk_calcs$`pijk Bit Structure 3`
 
   pijk_calcs$ratio <- pijk_calcs$fraction / pijk_calcs$expected_fraction
 
-  return(list(model = fit_MCMC,
-              model_sizes = msz_by_iter_MCMC,
-              vars_in_iterations = ni_in_model_MCMC_structures_ordered,
-              ij_interactions = pij_calcs,
-              ijk_interactions = pijk_calcs))
+  return(list(
+    model = MCMC_results,
+    model_sizes = msz_by_iter_MCMC,
+    vars_in_iterations = ni_in_model_MCMC_structures_ordered,
+    ij_interactions = pij_calcs,
+    ijk_interactions = pijk_calcs
+  ))
 }
---
+++ R/04_plot_MCMC_results.R
@@ -0,0 +1,160 @@
+#' Run logistic singular value decomposition on the binary molecular features
+#'
+#' This function takes in a binary matrix, assuming that the variable names include the fingerprint name such as "Pubchem"
+#'
+#' @param receptor_type Used for the kmeans plot from the SVD results, this is a characer string such as "Androgen"
+#' @param data matrix or dataframe that has all binary data except for possibly Name, Label_names, and Label, which are removed
+#' @param rank rank of the SVD that decomposes the binary molecular feature data before using the PCs in kmeans.
+#' @return Results from the logistic SVD
+#' @return Plot of Kmeans for visual inspection before creating clusters
+#' @export
+#'
+plot_model_size <- function(MCMC_results, receptor) {
+
+  MCMC_model_sizes <- MCMC_results$model_sizes
+  model_size_plot <- ggplot(MCMC_model_sizes, aes(x=size, y=number)) +
+    geom_bar(stat = "identity", aes(fill = receptor),
+             position = "dodge") +
+    xlab("MCMC Model Sizes") + ylab("Count") +
+    ggtitle(paste("MCMC Logic Regression for ", receptor, sep = "")) +
+    theme_bw()
+
+  return(model_size_plot)
+}
+
+
+plot_compare_model_size <- function(MCMC_results_1, MCMC_results_2, receptor_1, receptor_2) {
+
+  MCMC_model_1_sizes <- MCMC_results_1$model_sizes
+  MCMC_model_2_sizes <- MCMC_results_2$model_sizes
+
+  MCMC_model_1_sizes$receptor <- receptor_1
+  MCMC_model_2_sizes$receptor <- receptor_2
+
+  combind_receptor_model_sizes <- rbind(MCMC_model_1_sizes, MCMC_model_2_sizes)
+
+  model_size_plot <- ggplot(combind_receptor_model_sizes, aes(x=size, y=number)) +
+    geom_bar(stat = "identity", aes(fill = receptor),
+             position = "dodge") +
+    xlab("MCMC Model Sizes") + ylab("Count") +
+    ggtitle(paste("Comparing MCMC Model Sizes for ", receptor_1, " and ", receptor_2, sep = "")) +
+    theme_bw()
+
+  return(model_size_plot)
+}
+
+plot_variables_in_MCMC <- function(MCMC_results, receptor, top = 20, iters = 200000) {
+
+  vars_in_MCMC_iters <- MCMC_results$vars_in_iterations
+  vars_in_MCMC_iters$receptor <- receptor
+
+  var_use_top <- vars_in_MCMC_iters[order(-vars_in_MCMC_iters$ni_in_model_MCMC),]
+  var_use_top <- head(var_use_top, top)
+
+  var_use_plot <- ggplot(var_use_top) +
+    geom_point(aes(x = fraction, y = reorder(Bit_Substructure, fraction),
+                   colour = factor(receptor) )) +
+    xlab(paste("Fraction of", iters)) +
+    ylab("Bit Substructure") +
+    labs(colour="Receptor") +
+    ggtitle(paste("Top 20 Molecular Structures to Fit the ", receptor, "Receptor", sept = ""))
+
+  return(var_use_plot)
+}
+
+plot_compare_vars_in_MCMC <- function(MCMC_results_1, MCMC_results_2, receptor_1, receptor_2, top = 20, iters = 200000) {
+
+  recpt_one_vars_in_MCMC_iters <- MCMC_results_1$vars_in_iterations
+  recpt_one_vars_in_MCMC_iters$receptor <- receptor_1
+
+  recpt_two_vars_in_MCMC_iters <- MCMC_results_2$vars_in_iterations
+  recpt_two_vars_in_MCMC_iters$receptor <- receptor_2
+
+  recpt_one_var_use_top <- recpt_one_vars_in_MCMC_iters[order(-recpt_one_vars_in_MCMC_iters$ni_in_model_MCMC),]
+  var_use_top_recpt1 <- head(recpt_one_var_use_top, top)
+
+  recpt_two_var_use_top <- recpt_two_vars_in_MCMC_iters[order(-recpt_two_vars_in_MCMC_iters$ni_in_model_MCMC),]
+  var_use_top_recpt2 <- head(recpt_two_var_use_top, top)
+
+  rept_one_top_var_use_against_rept_two_merge <- rbind(var_use_top_recpt1,
+                                             recpt_two_vars_in_MCMC_iters[recpt_two_vars_in_MCMC_iters$Bit_Substructure %in% var_use_top_recpt1$Bit_Substructure,])
+
+  rept_two_top_var_use_against_rept_one_merge <- rbind(var_use_top_recpt2,
+                                                       recpt_one_vars_in_MCMC_iters[recpt_one_vars_in_MCMC_iters$Bit_Substructure %in% var_use_top_recpt2$Bit_Substructure,])
+
+
+  top_rept1_vars_against_rept2_plot <- ggplot(rept_one_top_var_use_against_rept_two_merge) +
+    geom_point(aes(x = fraction, y = reorder(Bit_Substructure, fraction),
+                   colour = factor(receptor) )) +
+    xlab(paste("Fraction of ", iters, " iterations")) +
+    ylab("Bit Substructure") +
+    labs(colour="Receptor") +
+    ggtitle(paste("Top 20 Molecular Structures to Fit ", receptor_1))
+
+  top_rept2_vars_against_rept1_plot <- ggplot(rept_two_top_var_use_against_rept_one_merge) +
+    geom_point(aes(x = fraction, y = reorder(Bit_Substructure, fraction),
+                   colour = factor(receptor) )) +
+    xlab(paste("Fraction of ", iters, " iterations")) +
+    ylab("Bit Substructure") +
+    labs(colour="Receptor") +
+    ggtitle(paste("Top 20 Molecular Structures to Fit ", receptor_2))
+
+
+
+  return(list(top_receptor_1_agst_2 = top_rept1_vars_against_rept2_plot,
+              top_receptor_2_agst_1 = top_rept2_vars_against_rept1_plot))
+}
+
+plot_compare_vars_subgroups <- function(MCMC_results, receptor, top = 20, iters = 200000) {
+
+  subgroup_var_grabber <- function(x, y){
+    recpt_one_vars_in_MCMC_iters <- x$vars_in_iterations
+    recpt_one_vars_in_MCMC_iters$group <- y
+    return(recpt_one_vars_in_MCMC_iters)
+  }
+
+  subgroup_vars_collapsed <- imap_dfr(.x = MCMC_results, .f = subgroup_var_grabber, .id = NULL)
+
+  get_top <- function(x){
+    group <- unique(x$group)
+    target_group_ordered <- x[order(-x$ni_in_model_MCMC),]
+    target_group_top <- head(target_group_ordered, top)
+    subgroup_vars_collapsed_filtered <- subgroup_vars_collapsed %>% filter(group != !!(group))
+
+    target_group_top_vs_rest <- rbind(target_group_top,
+                                      subgroup_vars_collapsed_filtered[subgroup_vars_collapsed_filtered$Bit_Substructure %in% target_group_top$Bit_Substructure,])
+
+    target_group_top_vs_rest_plot <- ggplot(target_group_top_vs_rest) +
+      geom_point(aes(x = fraction, y = reorder(Bit_Substructure, fraction),
+                     colour = factor(group) )) +
+      xlab(paste("Fraction of ", iters, " iterations")) +
+      ylab("Bit Substructure") +
+      labs(colour="Receptor") +
+      ggtitle(paste("Top 20 Molecular Structures to Fit Group", group, "vs Rest"))
+
+    return(target_group_top_vs_rest_plot)
+  }
+
+  subgroup_top_targets_vs_all_plots <-by(subgroup_vars_collapsed, INDICES = subgroup_vars_collapsed[,'group'], get_top)
+
+  return(target_subgroup_plot_comparisons = subgroup_top_targets_vs_all_plots)
+}
+
+extract_MCMC_couples_triples <- function(MCMC_results, receptor, couple_thresh, triple_thresh){
+  data_ij_interaction <- MCMC_results$ij_interactions
+  data_ijk_interaction <- MCMC_results$ijk_interactions
+
+  data_ij_interaction$receptor <- receptor
+  data_ijk_interaction$receptor <- receptor
+
+  data_ij_interactions_top <- subset(data_ij_interaction, fraction > couple_thresh)
+  data_ijk_interactions_top <- subset(data_ijk_interaction, fraction > triple_thresh)
+
+
+  return(list(MCMC_couples = data_ij_interactions_top,
+               MCMC_triples = data_ijk_interactions_top))
+
+}
+
+
+
